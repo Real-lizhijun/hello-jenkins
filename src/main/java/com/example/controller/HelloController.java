6087ac159d0f4139af310378ef2c317e9bcc188b
Real-lizhijun <422806951@qq.com>
2023-10-09 08:51:02 +0800

    feature: HelloController

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * hello Controller
+ *
+ * @author: lizhijun
+ * @date: 2023-10-09 8:47:55
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
